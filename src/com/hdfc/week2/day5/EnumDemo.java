8694ca7bcee70dc2327afecfbbf0aa1b96152d64
vinayak-rajput <vk.ra.org@gmail.com>
2026-08-12 21:42:08 +0530

    Added files for day3 of week3

---
+++ src/com/hdfc/week2/day5/EnumDemo.java
@@ -0,0 +1,15 @@
+public enum Days{
+    MON(10);
+    TUES(20);
+    WEDS(30);
+    THURS(40);
+    FRI(50);
+
+    Days(int val){
+        this.val  = val;
+    }
+
+    public int getValue(Days day){
+        return
+    }
+}
