d22ae20906758dc59e66a851d0ac4872f5e4a48a
vinayak-rajput <vk.ra.org@gmail.com>
2026-09-13 21:08:03 +0530

    Added Project Repo Links for Week7

--- src/com/hdfc/Week2/day5/EnumDemo.java
+++ src/com/hdfc/Week2/day5/EnumDemo.java
@@ -1,15 +1,17 @@
 public enum Days{
-    MON(10);
-    TUES(20);
-    WEDS(30);
-    THURS(40);
+    MON(10),
+    TUES(20),
+    WEDS(30),
+    THURS(40),
     FRI(50);
+
+    private final int val;
 
     Days(int val){
         this.val  = val;
     }
 
     public int getValue(Days day){
-        return
+        return val;
     }
 }
